bc9041f2acf73d29171f655d910aab82744c56f1
mengfei.xmf <mengfei.xmf@alibaba-inc.com>
2014-08-26 10:21:00 +0800

    update

---
+++ src/com/android/carair/request/ActivityInfoClickReuqest.java
@@ -0,0 +1,36 @@
+package com.android.carair.request;
+
+import com.android.carair.common.CarairConstants;
+import com.android.carair.net.ApiProperty;
+import com.android.carair.net.BizRequest;
+
+public class ActivityInfoClickReuqest extends BizRequest {
+    public ActivityInfoClickReuqest(String requestJson){
+        try {
+            this.postData = requestJson.getBytes("utf-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public ActivityInfoClickReuqest(byte[] requestJson){
+        try {
+            this.postData = requestJson;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    @Override
+    protected void setupApiProperty()
+    {
+        mApiProperty = new ApiProperty();
+        mApiProperty.setPost(true);
+        setCommonHeaders(mApiProperty);
+    }
+    
+    @Override
+    public String getApiUrl()
+    {
+        return CarairConstants.REQUEST_URL+"/app/activityclick";
+    }
+}
---
+++ src/com/android/carair/wxapi/WXEntryActivity.java
@@ -0,0 +1,7 @@
+
+package com.android.carair.wxapi;
+
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+public class WXEntryActivity extends WXCallbackActivity {
+}
